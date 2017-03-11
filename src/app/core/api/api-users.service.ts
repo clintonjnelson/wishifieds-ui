936777c58380e33fd938dba4f36e71feb37f92c4
clintonjnelson <clintonjnelson@live.com>
2017-03-11 14:09:50 -0800

    finish out API /users get by ID route and responses

--- src/app/core/api/api-users.service.ts
+++ src/app/core/api/api-users.service.ts
@@ -7,6 +7,20 @@ import { UserCreds, User } from '../../users/user.model';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
+// TODO: CREATE OBJECTS FOR THE RESPONSES ONCE THEYRE DIALED IN
+class CreateUserResponse {
+  eat: string;
+  username: string;
+  role: string
+}
+
+class UserByIdResponse {
+  username:  string;
+  email:     string;
+  role:      string;
+  confirmed: boolean;
+};
+
 
 @Injectable()
 
@@ -17,18 +31,34 @@ export class ApiUsersService {
   // Create a new User Account
   createUser(creds: UserCreds): Observable<any> {
     let createUserUrl = this.signpostApi.routes.createUser;
-    let headers       = this.signpostApi.headers.contentType.appJson;
-    let options       = new RequestOptions({headers: headers});
+    let header        = this.signpostApi.headers.contentType.appJson;
+    let options       = new RequestOptions({headers: header});
 
     console.log("DATA TO SEND IS: ", JSON.stringify(creds));
     return this.http
                .post(createUserUrl, JSON.stringify(creds), options)
-               .map( (res) =>  {
-                 console.log("RESPONSE IS: ", res.json());
-                 return res.json();
+               .map( user =>  {
+                 console.log("RESPONSE FROM USER CREATION IS: ", user.json());
+                 return user.json();
                })
                .catch( (error: Response | any) => {
-                 console.log("ERROR IS: ", error);
+                 console.log("ERROR FROM USER CREATION IS: ", error);
+                 return error;
+                 // show error message to user
+                 // Maybe use remote logging infrastructure
+               });
+  }
+
+  getUserById(userid: string): Observable<any> {
+    let getUserUrl = this.signpostApi.buildUrl('getUserById', [ {':id': userid} ] );
+    return this.http
+               .get(getUserUrl)
+               .map( user => {
+                 console.log("RESPONSE FROM GET USER BY ID IS: ", user.json());
+                 return user.json() as UserByIdResponse;
+               })
+               .catch( (error: Response | any) => {
+                 console.log("ERROR FROM GET USER BY ID IS: ", error);
                  return error;
                  // show error message to user
                  // Maybe use remote logging infrastructure
