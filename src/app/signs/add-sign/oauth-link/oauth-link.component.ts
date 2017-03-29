05da2fb2e1bc52863343d8e3e9ffedb890368f8c
clintonjnelson <clintonjnelson@live.com>
2017-03-29 13:00:43 -0700

    fix cookies on oauth1 by expiring 8sec after use

--- src/app/signs/add-sign/oauth-link/oauth-link.component.ts
+++ src/app/signs/add-sign/oauth-link/oauth-link.component.ts
@@ -27,7 +27,13 @@ export class OauthLinkComponent {
     // Oauth2 token requires eat token in query
     switch (this.icon) {
       case 'twitter': {
-        document.cookie = 'oauth1eat=' + eatToken;
+        let expDate = new Date();
+        expDate.setTime(expDate.getTime() + 8000);
+        // console.log("COOKIE EAT TOKEN IS: ", eatToken);
+        // console.log("COOKIE EXP DATE IS: ", expDate.toUTCString());
+        document.cookie = 'oauth1eat=' + eatToken + '; expires=' + expDate.toUTCString() +'; path=/';
+        // console.log("READING COOKIE TO CHECK IF EXPIRED: ", readCookie('oauth1eat'));
+        // console.log("UPDATED COOKIES OBJECT IS: ", document.cookie);
         // I don't think Oauth1 needs the query. Won't use it.
         window.location.href = this.url + '?signType=' + this.icon;
         break;
@@ -38,5 +44,16 @@ export class OauthLinkComponent {
                                 '&signType=' + this.icon;
       }
     }
+
+    // function readCookie(name) {
+    //   var nameEQ = name + "=";
+    //   var ca = document.cookie.split(';');
+    //   for(var i=0;i < ca.length;i++) {
+    //     var c = ca[i];
+    //     while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    //     if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    //   }
+    //   return null;
+    // }
   }
 }
