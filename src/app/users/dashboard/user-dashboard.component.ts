8f659b8f8171bc8291f28977e926bfdc9a9e9f30
clintonjnelson <clintonjnelson@live.com>
2017-05-26 06:15:41 -0700

    fix graph logic to show all days/values in range

--- src/app/users/dashboard/user-dashboard.component.ts
+++ src/app/users/dashboard/user-dashboard.component.ts
@@ -11,8 +11,8 @@ import { ApiDashboardService } from '../../core/api/api-dashboard.service';
 
 
 export class UserDashboardComponent implements OnInit {
-  line: any;
-  chartData: any;
+  chartData:  any;
+  dateCounts: any;
   isProcessing: boolean = true;
 
   constructor(private apiDashboardService: ApiDashboardService) {
@@ -20,7 +20,8 @@ export class UserDashboardComponent implements OnInit {
       console.log("SETTING CHART COLORS");
       (<any>window).Chart.defaults.global.defaultFontColor = '#ffffff';
     }
-    this.chartData = {};
+    this.dateCounts = {};
+    this.chartData  = {};
   }
 
   ngOnInit() {
@@ -31,6 +32,8 @@ export class UserDashboardComponent implements OnInit {
         .subscribe( interactions => {
           console.log("INTERACTIONS RETURNED: ", interactions);
           this.setChart();
+          this.resetDateCounts(interactions);
+          console.log("COUNTS ARE: ", this.dateCounts);
           this.buildLine(interactions);
           this.isProcessing = false;
           console.log("CHART DATA AFTER FORMATTING IS: ", that.chartData);
@@ -52,16 +55,17 @@ export class UserDashboardComponent implements OnInit {
                                 },
                                 scales: {
                                   xAxes: [{
-                                    // type: 'time',
-                                    // time: {
-                                    //   displayFormat: {
-                                    //     day: 'YYYY-MM-DD',
-                                    //     max: new Date().toISOString().substring(0,10),
-                                    //   }
-                                    // },
+                                    type: 'time',
+                                    time: {
+                                      unit: 'day',
+                                      displayFormats: {
+                                        day: 'YYYY-MM-DD',
+                                        max: new Date().toISOString().substring(0,10),
+                                      }
+                                    },
                                     gridLines: {
                                       display: true,
-                                      color: '#C0C0C0'
+                                      color: '#c0c0c0'
                                     }
                                   }],
                                   yAxes: [{
@@ -82,7 +86,7 @@ export class UserDashboardComponent implements OnInit {
     this.chartData.type    = 'line';
     this.chartData.legend  = true;
     this.chartData.colors  = [{
-      backgroundColor: 'rgba(148,159,177,0.2)',
+      backgroundColor: '#d3d3d3',
       borderColor: 'rgba(255,255,255,0)',
       pointBackgroundColor: 'rgba(148,159,177,1)',
       pointBorderColor: '#fff',
@@ -93,43 +97,51 @@ export class UserDashboardComponent implements OnInit {
 
   buildLine(apiInteractions) {
     var that = this;
-    let prevDate = '';
+    // let prevDate = '';
+    // apiInteractions.forEach(function(dataPoint) {
+    //   const date = dataPoint.createdAt.substring(0, 10);
+    //   // If exists, increment count by one
+    //   if(date === prevDate) {
+    //     const countIndex = that.chartData.line[0].data.length - 1;
+    //     that.chartData.line[0].data[countIndex]++;  //verify increments
+    //   }
+    //   else {
+    //     that.chartData.labels.push(date);     // add newest label
+    //     that.chartData.line[0].data.push(1);  // add newest count
+    //     prevDate = date;
+    //   }
+    // });
+
+    // Build count object
     apiInteractions.forEach(function(dataPoint) {
       const date = dataPoint.createdAt.substring(0, 10);
-      // If exists, increment count by one
-      if(date === prevDate) {
-        const countIndex = that.chartData.line[0].data.length - 1;
-        that.chartData.line[0].data[countIndex]++;  //verify increments
-      }
-      else {
-        that.chartData.labels.push(date);     // add newest label
-        that.chartData.line[0].data.push(1);  // add newest count
-        prevDate = date;
-      }
+      that.dateCounts[date]++;
+      // dateCounts[date] = (currCount ? currCount++ : 1);
+    });
+    // set labels in order
+    // this.chartData.labels = Object.keys(dateCounts).sort();
+    // set point counts in same order as labels
+    this.chartData.labels.forEach(function(date) {
+      that.chartData.line[0].data.push(that.dateCounts[date]);
     });
 
-    // dateCounts: {};
-    // // Build count object
-    // apiDataPointsArr.forEach(function(dataPoint) {
-    //   const date       = dataPoint.createdAt.substring(0, 10);
-    //   const currCount  = dateCounts[date];
+  }
 
-    //   dateCounts[date] = (currCount ? currCount++ : 1);
-    // });
-    // // set labels in order
-    // this.chartData.labels = Object.keys(dateCounts).sort();
-    // // set point counts in same order as labels
-    // this.chartData.labels.forEach(function(date) {
-    //   this.chartData.line[0].data.push(dateCounts[date]);
-    // });
+  resetDateCounts(apiInteractions) {
+    const startDateStr = new Date(apiInteractions[0].createdAt).toISOString().substring(0, 10);
+    const endDate      = new Date(apiInteractions[apiInteractions.length - 1]['createdAt']);
+    const stopStr      = new Date(endDate.setDate(endDate.getDate() + 1)).toISOString().substring(0, 10); // include end date in while loop
 
-    // SINCE MONGO IS SORTED CAN DO THIS:
-      // add first label to the labels
-      // add label count of one to data
-      // record this label for checking
-      // move to next label
-          // if same as first label, only increment data count
-          // if new label, add to labels, add 1 to data
+    let currDateStr = startDateStr;  // initial value is first date
+    while(currDateStr < stopStr) {
+      console.log("DATE IS NOW: ", currDateStr);
+      this.dateCounts[currDateStr] = 0;                                      // set object of all counts
+      this.chartData.labels.push(currDateStr);                               // push in each date for x-axis
+
+      let date = new Date(currDateStr);
+      currDateStr = new Date(date.setDate(date.getDate() + 1)).toISOString().substring(0, 10);  // increment
+    }
+    console.log("BLANK DATE COUNT BUILT: ", this.dateCounts);
   }
 
 
